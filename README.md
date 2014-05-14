e5eb59be91c9c0145836554f6315684de59bfff2
bkinder42 <ben@victen.org>
2014-05-14 12:05:16 -0400

    Updated readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 JavaBoardGame
 =============
 
-A simple board game project for my Java class.
+A simple board game project for my Java class. Documentation can be found at https://victen.org/chess/ or in the JavaDoc included in the program download.
--- src/project/Piece/Piece.java
+++ src/project/Piece/Piece.java
@@ -25,29 +25,48 @@ public abstract class Piece {
         }
     }
 
+    /**
+     * Returns the color of the Piece
+     * @return The color of the Piece as a Java Color Object.
+     */
     public Color getColor() {
         return color;
     }
 
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
+    /**
+     * Returns the location of the Piece on the Board.
+     * @return The location of the Piece on the board as a Location Object
+     * @see project.Resources.Location
+     */
     public Location getLocation() {
         return location;
     }
 
+    /**
+     * Sets the location of the Piece on the Board.
+     * @param location the location on the Board to move the Piece to as a Location Object.
+     * @see project.Resources.Location
+     */
     public void setLocation(Location location) {
         this.location = location;
     }
 
+    /**
+     * Returns a boolean telling if the Piece is white.
+     * @return A boolean, if true the Piece is white, if false the Piece is black.
+     */
     boolean isWhite() {
         return white;
     }
 
+    /**
+     * Returns the 'symbol' to be used by the CLI class to represent the piece.
+     * @return String representing the Piece to be used by the CLI Class.
+     */
     public String getSymbol(){
         return symbol;
     }
+
 
     @Override
     public String toString() {
